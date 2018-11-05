504e6e5ec7de6830a8550f269bad5f5667cd4747
Winston Peng <winston.peng@stuypulse.com>
2018-11-05 18:28:45 -0500

    Add drivetrain subsystem and the DrivetrainDriveCommand. Add Gamepad.java class, and the basic Java Robot project structure

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # NoobieNewbieRobot
-"nothing in here will reveal very secret information"
+
+This is a practice robot that the newbies will be creating in order to practice their coding skillz. It is an imitation of our 2017 Rafael robot. Each newbie will be working on a separate subsystem and practicing their github skills in order to work together to complete a robot.
+
+## Requirements For Subsystems
+
+- Instance of subsystem class inside Robot.java
+- Port numbers for the necessary variables must be included in RobotMap.java
+- Variables are initialized in the class, but defined within the constructor.
+- Add DefaultCommand when necessary
+
+## Requirements For Commands
+
+- requires(subsystem) statement in the constructor
+- initialize() if the command runs once, and execute() if the command will run repeatedly.
+
+## Drivetrain
+
+This is the first subsystem. It was coded together. It includes the methods initDefaultCommand(), tankdrive(), and getSpeed(). It includes four WPI_TalonSRX motors, two SpeedController variables, and one DifferentialDrive variable. The commands are DrivetrainDriveCommand (mapped to the joysticks on the driverpad).
---
+++ src/org/usfirst/frc/team694/robot/subsystems/Drivetrain.java
@@ -0,0 +1,61 @@
+package org.usfirst.frc.team694.robot.subsystems;
+
+import org.usfirst.frc.team694.robot.RobotMap;
+import org.usfirst.frc.team694.robot.commands.DrivetrainDriveCommand;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ *
+ */
+public class Drivetrain extends Subsystem {
+
+    private WPI_TalonSRX leftTopMotor;
+    private WPI_TalonSRX rightTopMotor;
+    private WPI_TalonSRX leftBottomMotor;
+    private WPI_TalonSRX rightBottomMotor;
+    
+    private SpeedControllerGroup leftMotors;
+    private SpeedControllerGroup rightMotors;
+    
+    private DifferentialDrive differentialDrive;
+
+    public Drivetrain() {
+    	leftTopMotor = new WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_TOP_MOTOR);
+    	rightTopMotor = new WPI_TalonSRX(RobotMap.DRIVETRAIN_RIGHT_TOP_MOTOR);
+    	leftBottomMotor = new WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_BOTTOM_MOTOR);
+    	rightBottomMotor = new WPI_TalonSRX(RobotMap.DRIVETRAIN_RIGHT_BOTTOM_MOTOR);
+    	
+    	leftMotors = new SpeedControllerGroup(leftTopMotor, leftBottomMotor);
+    	rightMotors = new SpeedControllerGroup(rightTopMotor, rightBottomMotor);
+    	
+    	differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+    }
+    
+    public void initDefaultCommand() {
+        setDefaultCommand(new DrivetrainDriveCommand());
+    }
+    
+    public double getLeftSpeed() {
+    	return leftTopMotor.get();
+    }
+    
+    public double getRightSpeed() {
+    	return rightTopMotor.get();
+    }
+    
+    public double getSpeed() {
+    	return Math.max(Math.abs(getLeftSpeed()), Math.abs(getRightSpeed()));
+    }
+    
+    public void tankDrive(double leftspeed, double rightspeed) {
+    	differentialDrive.tankDrive(leftspeed, rightspeed);
+    }
+    
+    
+}
+
