f441d93fe7b1faa2f24a1bbe4e25b6532cb71c77
Timothy So <so.timothy51@gmail.com>
2018-11-13 23:40:10 -0500

    formatting; changing of letter case (could raise errors later, preventing)

--- src/org/usfirst/frc/team694/robot/subsystems/Winch.java
+++ src/org/usfirst/frc/team694/robot/subsystems/Winch.java
@@ -9,28 +9,28 @@ import org.usfirst.frc.team694.robot.commands.startWinchSlow;
 import org.usfirst.frc.team694.robot.commands.stopWinch;
 
 public class Winch extends Subsystem {
+
 	private WPI_TalonSRX winchMotor;
-	
-	public Winch(){
+
+	public Winch() {
 		winchMotor = new WPI_TalonSRX(RobotMap.WINCHMOTOR);
 	}
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
-    public void initDefaultCommand() {
-    	setDefaultCommand(new stopWinch());
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-    
-    public void startWinchFast(){
-    	winchMotor.set(1.0);
-    }
-    public void startWinchSlow(){
-    	winchMotor.set(0.3);
-    }
-    public void stopWInch(){
-    	winchMotor.set(0.0);
-    }
+	// Put methods for controlling this subsystem here. Call these with commands.
+  public void initDefaultCommand() {
+  	setDefaultCommand(new stopWinch());
+    // default command for a subsystem
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void startWinchHigh() {
+  	winchMotor.set(1.0);
+  }
+  public void startWinchLow() {
+  	winchMotor.set(0.3);
+  }
+  public void stopWinch() {
+  	winchMotor.set(0.0);
+  }
+
 }
-
